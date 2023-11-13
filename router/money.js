1b522825d8a575f64b633283c3425055e1ba574d
Mr.Khawaja <alveehawak360@gmail.com>
2023-11-14 05:23:57 +0600

    added transaction history and payment option

--- router/money.js
+++ router/money.js
@@ -146,4 +146,61 @@ app.post("/cashout", auth, (req, res) => {
   );
 });
 
+app.post("/pay", auth, (req, res) => {
+  const phone = req.decoded.phone;
+  const { amount, receiver } = req.body;
+  db.query(
+    "select balance from users where phone = ?",
+    [phone],
+    (err, result) => {
+      if (err) throw err;
+      const balance = result[0].balance;
+      if (balance < amount) return res.status(400).send("Insufficient balance");
+      db.query(
+        "select * from users where phone = ? and type = 'merchant'",
+        [receiver],
+        (err, result) => {
+          if (err) throw err;
+          if (result.length <= 0)
+            return res.status(400).send("Merchant not found");
+          db.query(
+            "update users set balance = balance - ? where phone = ?",
+            [amount, phone],
+            (err, result) => {
+              if (err) throw err;
+              db.query(
+                "update users set balance = balance + ? where phone = ?",
+                [amount, receiver],
+                (err, result) => {
+                  if (err) throw err;
+                  db.query(
+                    "insert into transactions (sender,receiver,amount,type) values (?,?,?,?)",
+                    [phone, receiver, amount, "pay"],
+                    (err, result) => {
+                      if (err) throw err;
+                      res.send("Payment successful");
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+app.get("/transactions", auth, (req, res) => {
+  const phone = req.decoded.phone;
+  db.query(
+    "select * from transactions where sender = ? or receiver = ? limit 10",
+    [phone, phone],
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+});
+
 module.exports = app;
